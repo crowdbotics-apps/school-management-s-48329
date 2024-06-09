2783d5c2d83032287a7a1b71dd8853269e3728b7
Crowdbotics <team@crowdbotics.com>
2024-06-09 14:34:21 +0000

    Initial commit

---
+++ backend/school_management_s_48329/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for school_management_s_48329 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "school_management_s_48329.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "school-management-s-48329",
+  url: "https://school-management-s-48329.botics.co",
+  api: "https://school-management-s-48329.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
